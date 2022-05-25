f4c92988c2a55d1763155e80cbcd802e485a99b4
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-24 17:20:53 -0700

    created ingredients SQL table

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS recipes CASCADE;
+DROP TABLE IF EXISTS ingredients CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -13,7 +14,17 @@ CREATE TABLE users (
 CREATE TABLE recipes (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name TEXT NOT NULL,
-  ingredient_ids BIGINT[],
+  ingredient_ids BIGINT [] REFERENCES ingredients (id),
   user_id BIGINT REFERENCES users (id),
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE ingredients (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  common_name TEXT NOT NULL,
+  scientific_name TEXT NOT NULL,
+  image TEXT NOT NULL,
+  type TEXT NOT NULL,
+  health_benefits TEXT [] NOT NULL,
+  description TEXT
+)
