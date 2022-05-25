936dec2e4375deb81c9c2acc4b00c5291ed5d84c
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-24 17:06:50 -0700

    created users SQL table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL,
+  username TEXT NOT NULL
+);
