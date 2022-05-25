b7ba883bb0442c6bb930bf6b548ffc36ad3abff9
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-24 17:17:25 -0700

    created recipes SQL table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,10 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS recipes CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email TEXT NOT NULL,
   password_hash TEXT NOT NULL,
   username TEXT NOT NULL
+);
+
+CREATE TABLE recipes (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL,
+  ingredient_ids BIGINT[],
+  user_id BIGINT REFERENCES users (id),
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
