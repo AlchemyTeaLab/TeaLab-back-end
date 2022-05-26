8092638adc31c91a6725910f5a281416e9395b25
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-26 10:36:35 -0700

    added ingredients controller/model/test to file tree

---
+++ __tests__/ingredients.test.js
@@ -0,0 +1,15 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('TeaLab-back-end routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+});
