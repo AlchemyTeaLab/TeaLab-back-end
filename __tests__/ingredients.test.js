567c0c0cf7b9698e64076a380e39b833324e11b3
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-26 10:43:49 -0700

    wrote getAll test for ingredients route

--- __tests__/ingredients.test.js
+++ __tests__/ingredients.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Ingredient = require('../lib/models/Ingredient');
 
 describe('TeaLab-back-end routes', () => {
   beforeEach(() => {
@@ -12,4 +13,16 @@ describe('TeaLab-back-end routes', () => {
     pool.end();
   });
 
+  it.skip('should allow admin to POST an ingredient', async () => {
+
+  });
+
+  it.skip('should get a list of ingredient', async () => {
+    const expected = await Ingredient.getAllIngredients();
+
+    const res = await request(app)
+      .get('/api/v1/ingredients');
+
+    expect(res.body).toEqual(expected);
+  });
 });
